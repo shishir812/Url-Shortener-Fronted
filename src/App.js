87ec613301c333e91c522dc7ed3e62c37e0c07f3
shishir <49905610+shishir812@users.noreply.github.com>
2022-09-10 20:18:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [longurl, setLongurl] = useState("");
+  const [shorturl, setShorturl] = useState("");
+  const [returnLongURL, setReturnLongURL] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("http://127.0.0.1:8000/shorten/", {
+      method: "POST",
+      body: JSON.stringify({ longurl: longurl }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setShorturl(data.shorturl);
+        setReturnLongURL(data.longurl);
+        setLongurl("");
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ textAlign: "center" }}>
+      <input
+        type="text"
+        name="longurl"
+        value={longurl}
+        onChange={(e) => setLongurl(e.target.value)}
+      />
+      <button
+        type="submit"
+        onClick={(e) => handleSubmit(e)}
+        disabled={!longurl}
+      >
+        shorten
+      </button>
+
+      <div>
+        <p>Long URL: {returnLongURL}</p>
+        <p
+          style={{ cursor: "pointer" }}
+          onClick={() => window.open(returnLongURL)}
         >
-          Learn React
-        </a>
-      </header>
+          Short URL: {shorturl}
+        </p>
+      </div>
     </div>
   );
 }
